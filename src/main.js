8fd07d5171fa702594fc2cb78b102bc1d737fb71
Wincent <wincenttsjoi@gmail.com>
2020-05-16 01:45:00 +0800

    added bootstrap

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VCalendar from 'v-calendar';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue);
 
 Vue.use(VCalendar);
 
