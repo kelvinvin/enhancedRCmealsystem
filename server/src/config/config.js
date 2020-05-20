3e183ccf688e5a67f1cef603f88dc388e36f5e4b
Wincent <wincenttsjoi@gmail.com>
2020-05-20 17:39:45 +0800

    backend add

---
+++ server/src/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    port: process.env.PORT || 8081,
+    db: {
+        database: process.env.DB_NAME || 'enhancedRCmealsystem', 
+        user: process.env.DB_USER || 'enhancedRCmealsystem',
+        password: process.env.DB_PASS || 'enhancedRCmealsystem',
+        options: {
+            dialect: process.env.DIALECT || 'sqlite',
+            host: process.env.HOST || 'localhost',
+            storage: './enhancedRCmealsystem.sqlite'
+        }
+    }
+}
