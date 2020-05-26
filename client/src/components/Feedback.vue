f433b19df461afdb065c08e8998752d4eaba9360
Wincent <wincenttsjoi@gmail.com>
2020-05-26 18:40:35 +0800

    feedback

--- client/src/components/Feedback.vue
+++ client/src/components/Feedback.vue
@@ -1,23 +1,26 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <p>Choose your date for feedback</p>
-        <date-picker v-model="date" :config="options"></date-picker>
-      </div>
-        <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Food type
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item">Malay</a>
-                <a class="dropdown-item">Another action</a>
-                <a class="dropdown-item">Something else here</a>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <p>Choose your date for feedback</p>
+                <date-picker v-model="date" :config="options"></date-picker>
             </div>
+            
+       
+      
+    </div>
+        <div>
+          <b-form-textarea
+            id="textarea"
+            v-model="text"
+            placeholder="Enter something..."
+            rows="7"
+            max-rows="30"
+            ></b-form-textarea>
         </div>
     </div>
-  </div>
 </template>
+
 
 <script>
   import 'bootstrap/dist/css/bootstrap.css';
@@ -32,6 +35,7 @@
           format: 'DD/MM/YYYY',
           useCurrent: false,
         },
+        text: '',
       }
     },
     components: {
@@ -39,10 +43,12 @@
     },
     methods: {
         
-    }
+    },
+    mounted() {
+    this.$root.$on('bv::dropdown::show', bvEvent => {
+      console.log('Dropdown is about to be shown', bvEvent)
+    })
+  }
   }
 </script>
 
-<style scoped>
-
-</style>
