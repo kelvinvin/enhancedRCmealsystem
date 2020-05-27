643bcaaecfb92e488d15c6ad48e2f7984f6b9312
Wincent <wincenttsjoi@gmail.com>
2020-05-27 15:52:53 +0800

    chancgtes

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -7,7 +7,6 @@
             <h3>Sign in with your organizational account</h3>
 
             <form>
-                <div class="error" v-html="error" />
                 <input type="text" class="username" v-model="email" placeholder="Enter your NUS email here...">
                 <input type="password" class="password" v-model="password" 
                 placeholder="Enter your password here...">
--- server/src/controllers/MealsController.js
+++ server/src/controllers/MealsController.js
@@ -5,19 +5,6 @@ module.exports = {
         try {
             const meals = await Meal.findAll()
             res.send(meals)
-            // const {date, link} = req.body
-            // const meals = await Meal.findOne({
-            //     where: {
-            //         date: date
-            //     }
-            // })
-            // if (!meals) {
-            //     return res.status(403).send({
-            //         error: 'meal not found for that date'
-            //     })
-            // }
-            // res.send(meals.link)
-
         } catch (err) {
             res.status(500).send({
                 error: 'An error has occured trying to fetch the meal'
