2bd7755946412f3a6360f2c7b7c4eaea3e2e4d08
Wincent <wincenttsjoi@gmail.com>
2020-05-29 16:23:39 +0800

    removed irrelevant stuff

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,10 +1,8 @@
 const AuthenticationController = require('./controllers/AuthenticationController')
-const AuthenticationControllerPolicy = require('./policies/AuthenticationContollerPolicy')
 const MealsController = require('./controllers/MealsController')
 
 module.exports = (app) => {
     app.post('/register', 
-       // AuthenticationControllerPolicy.register,
         AuthenticationController.register)
 
     app.post('/login',
